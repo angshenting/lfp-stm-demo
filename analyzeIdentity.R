4c054841ef635d9fc6575cf8dd6440d22cb4e41e
Shen Ting Ang <angshenting@gmail.com>
2016-02-10 18:13:16 -0800

    Cleaned up R file

--- analyzeIdentity.R
+++ analyzeIdentity.R
@@ -37,71 +37,19 @@ plot.STM(mod.out, xlim=c(0,.5), labeltype="prob", n=30)
 # Additional in STM, estimation over covariates
 prep <- estimateEffect(1:30~facebook + s(date), mod.out, metadata=out$meta)
 plot.estimateEffect(prep, "date", topics=26, method="continuous")
+axis(1, at=seq(14025,16222, by=365), labels=seq(2008,2014))
 
-#Topic 8: Entitlement topic
-findThoughts(mod.out, meta$Content, topics=8)
+# You can also plot over binary categories
+plot.estimateEffect(prep, "facebook", topics=26, method="pointestimate")
 
-#Topic 11 & 13 more recent topics.
-#Topic 11: Protests against foreigners
-#Topic 13: Migrant worker strike
-
-findThoughts(mod.out, meta$Content, topics=20, n=5)
+# Displays the top 5 documents belonging to topic 8
+findThoughts(mod.out, meta$Content, topics=8, n=5)
 
 
-#Topic 15: Analyses of Singaporean identity -- more at the beginning of the period
-#Topic 16: Racism awareness, small incidents of racism and how to deal with them.
-#Topic 18: Language -- increases over time
-#Topic 21: Speech by PM Lee
-#Topic 23: Riot
-#Topic 25: Advertisement to work on Singapore identity project
-#Topic 30: Sports
-
-#Talk about main topics of identity over 2008-2014 -- constant over time
-#Food,
-#Art,
-#Societal Values cluster
+# You can also plot multiple topics and add custom labels
 plot.estimateEffect(prep, "date", topics=c(26,4,12), method="continuous", xaxt="n", ylim=c(-.05, .4), labeltype="custom", custom.labels=c("Food",
                                                                                                                             "Societal Values", "Art"))
-axis(1, at=seq(14025,16222, by=365), labels=seq(2008,2014))
-findThoughts(mod.out, data$Content,topics=4,n=7)
 
-#"I am attempting to capture features of a neighbourhood which are typical of Singapore’s
-#heritage and tradition. Besides Shop Houses, I have also included old buildings,
-#famous food places, hawker centres and many more. I hope that after looking at it,
-#Singaporeans can see our country differently and realize the importance of heritage sites
-#as they define part of our Singaporean identity and core."
-
-
-#Talk about immigration worries, increasing over time
-#Foreigners & growth
-#Immigration
-#Riots
-#Labor Strikes
-#Xenophobia
-#Citizenship
-plot.estimateEffect(prep, "date", topics=c(11,13,18,8,16,27), method="continuous", xaxt="n", ylim=c(0, .35), labeltype="custom", custom.labels=rev(c("Singaporean Future", "Immigration Protests",
-                                                                                                                                    "Labor Strikes", "Xenophobia",
-                                                                                                                                   "Foreigners and Growth", "Citizenship")))
-axis(1, at=seq(14025,16222, by=365), labels=seq(2008,2014))
-findThoughts(mod.out, data$Content,topics=27,n=7)
-#"From concerns about smaller living spaces and the possible erosion of the Singaporean identity,
-#to whether the Republic will preserve its racial mix, the anxiety of some senior citizens
-#on the influx of foreigners was apparent during discussions yesterday as part of the
-#Our Singapore Conversation project"
-
-#History cluster: decreasing over time
-#Chinese Culture: 5
-#PAP: 2
-#Religion: 7
-#Singaporean History:22
-
-plot.estimateEffect(prep, "date", topics=c(2,5,7,22), method="continuous", xaxt="n", labeltype="custom", custom.labels=c("PAP", "Chinese Culture",
-                    "Religion", "Singaporean History"), ylim=c(-.1, .3))
-axis(1, at=seq(14025,16222, by=365), labels=seq(2008,2014))
-findThoughts(mod.out, data$Content,topics=5,n=7)
-#"Well, the government has started yet another push to elevate Chinese culture again over all'others'.
-#My question is, as it has always been, where does it leave the Malays and Indians?"
-findThoughts(mod.out, data$Content,topics=2,n=7)
 labels <- c("Sociological", "PAP", "Southeast Asia", "Food", "Chinese Culture",
             "Slang", "Religion", "Singaporean Future", "Racially Mixed Identity",
             "Elections", "Protests over Immigration", "Societal Values",
@@ -110,8 +58,9 @@ labels <- c("Sociological", "PAP", "Southeast Asia", "Food", "Chinese Culture",
             "Singaporean History", "Riots", "Hodge-podge", "Education Project", "Art",
             "Citizenship", "Immigration", "National Day", "Sports")
 
+# Calculates Topic Correlations
 corr <- topicCorr(mod.out, cutoff=0)
+# Plots graph of correlations
 plot.topicCorr(corr, vlabels=labels, vertex.color="lightblue")
 
-plot.estimateEffect(prep, "date", topics=c(21,28), method="continuous")
 
